86b6e1eb4d035ba850e074e32fceaae933d606fe
aspriest <adenpriest@icloud.com>
2023-05-10 22:27:29 +1000

    Hardcoded production server for FLEET. Removed unnecessary view buttons.

--- TraccarManager/StartViewController.swift
+++ TraccarManager/StartViewController.swift
@@ -14,21 +14,79 @@
 // limitations under the License.
 //
 
+//import UIKit
+//
+//class StartViewController: UIViewController, UITextFieldDelegate {
+//
+//    @IBOutlet weak var serverField: UITextField!
+//    @IBOutlet weak var startButton: UIButton!
+//
+//    @IBAction func onStart(_ sender: AnyObject) {
+//        startButton.isEnabled = false
+//
+//        var urlString = serverField.text!
+//        if !urlString.hasSuffix("/") {
+//            urlString += "/"
+//        }
+//        urlString += "api/server"
+//
+//        if let url = URL(string: urlString) {
+//            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+//                if error == nil {
+//                    do {
+//                        try JSONSerialization.jsonObject(with: data!, options: [])
+//                        DispatchQueue.main.async {
+//                            self.onSuccess()
+//                        }
+//                    } catch _ as NSError {
+//                        DispatchQueue.main.async {
+//                            self.onError()
+//                        }
+//                    }
+//                } else {
+//                    DispatchQueue.main.async {
+//                        self.onError()
+//                    }
+//                }
+//            }
+//            task.resume()
+//        } else {
+//            self.onError()
+//        }
+//    }
+//
+//    override func viewDidLoad() {
+//        serverField.delegate = self
+//    }
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.resignFirstResponder()
+//        return true
+//    }
+//
+//    func onSuccess() {
+//        UserDefaults.standard.set(serverField.text, forKey: "url")
+//        performSegue(withIdentifier: "StartSegue", sender: self)
+//    }
+//
+//    func onError() {
+//        startButton.isEnabled = true
+//
+//        let alert = UIAlertController(title: "Error", message: "Server connection failed", preferredStyle: UIAlertController.Style.alert)
+//        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default))
+//        present(alert, animated: true)
+//    }
+//
+//}
+
 import UIKit
 
-class StartViewController: UIViewController, UITextFieldDelegate {
-    
-    @IBOutlet weak var serverField: UITextField!
-    @IBOutlet weak var startButton: UIButton!
-    
-    @IBAction func onStart(_ sender: AnyObject) {
-        startButton.isEnabled = false
+class StartViewController: UIViewController {
 
-        var urlString = serverField.text!
-        if !urlString.hasSuffix("/") {
-            urlString += "/"
-        }
-        urlString += "api/server"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let urlString = "http://demo.traccar.org/api/server"
         
         if let url = URL(string: urlString) {
             let task = URLSession.shared.dataTask(with: url) { data, response, error in
@@ -54,27 +112,15 @@ class StartViewController: UIViewController, UITextFieldDelegate {
             self.onError()
         }
     }
-    
-    override func viewDidLoad() {
-        serverField.delegate = self
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
 
     func onSuccess() {
-        UserDefaults.standard.set(serverField.text, forKey: "url")
+        UserDefaults.standard.set("http://demo.traccar.org", forKey: "url")
         performSegue(withIdentifier: "StartSegue", sender: self)
     }
     
     func onError() {
-        startButton.isEnabled = true
-        
         let alert = UIAlertController(title: "Error", message: "Server connection failed", preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default))
         present(alert, animated: true)
     }
-
 }
